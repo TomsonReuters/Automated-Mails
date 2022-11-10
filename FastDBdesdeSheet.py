96ba7f4fb7d8d249c57705776800cf93f887ad7b
TomsonReuters <106933149+TomsonReuters@users.noreply.github.com>
2022-11-10 15:46:09 -0300

    Update and rename DBdesdeSheet.py to FastDBdesdeSheet.py

--- FastDBdesdeSheet.py
+++ FastDBdesdeSheet.py
@@ -1,9 +1,14 @@
 import sqlite3
 import pandas as pd
 
+#Crear la DB
+
 con = sqlite3.connect('StockTest.db')
+
+#Leer archivo con ruta especifica
 wb = pd.read_excel('/Users/tom/Desktop/StockDB.xlsx',sheet_name = None)
 
+#Añadirla a SQL
 for sheet in wb:
     wb[sheet].to_sql(sheet,con,index=False)
 con.commit()
